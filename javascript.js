06904805fd97f84c8ddc57089f7884653d1376d3
Caches <gfdominguez119@gmail.com>
2022-08-17 19:32:48 -0400

    Refactor code for web UI

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let computerWins = 0;
+let playerWins = 0;
+
 function getComputerChoice()
 {
     let randInt = Math.floor(Math.random() * 3);
@@ -13,44 +16,77 @@ function getComputerChoice()
     }
 }
 
-function getPlayerChoice(userChoice)
+function grantPoint(winner)
 {
-    userChoice = Number(userChoice);
-    switch (userChoice) 
-    {
+    switch (winner) {
+        case 0:
+            document.querySelector('#computerWins').textContent = `Computer: ${++computerWins}`;
+            break;
         case 1:
-            return "Rock";
+            document.querySelector('#playerWins').textContent = `Player: ${++playerWins}`;
+            break;
         case 2:
-            return "Paper";
-        case 3:
-            return "Scissors";
+            document.querySelector('#computerWins').textContent = `Computer: ${++computerWins}`;
+            document.querySelector('#playerWins').textContent = `Player: ${++playerWins}`;
+            break;
+        default:
+            break;
     }
 }
 
 function game(computerChoice, userChoice) 
 {
     switch (true) 
-    {
+    {   // if player wins return 1
         case computerChoice === "Rock" && userChoice === "Scissors":
         case computerChoice === "Paper" && userChoice === "Rock":
         case computerChoice === "Scissors" && userChoice === "Paper":
-            console.log(`Computer wins! Computer: ${computerChoice} You: ${userChoice}`);
-            break;
+            return 1;
 
         case userChoice === "Rock" && computerChoice === "Scissors":
         case userChoice === "Paper" && computerChoice === "Rock":
         case userChoice === "Scissors" && computerChoice === "Paper":
-            console.log(`Computer wins! Computer: ${computerChoice} You: ${userChoice}`);
-            break;
-
+            return 0;
+        // tie
         case computerChoice === userChoice:
-            console.log(`It's a tie! Computer: ${computerChoice} You: ${userChoice}`);
-            break;
-    
-        default:
-            console.log(`You can only input 1, 2 or 3. Try again`);
-            break;
+            return 2;
     }
 }
 
-game(getComputerChoice(), getPlayerChoice(prompt("Choose a number: \n1-Rock \n2-Paper \n3-Scissors")));
+
+const picker = document.querySelectorAll('.picker > p');
+for (let item of picker) 
+{
+    item.addEventListener('click', () =>
+    {
+        const userChoice = item.textContent.charAt(0).toUpperCase() + item.textContent.slice(1);
+        const gameResult = game(getComputerChoice(), userChoice);
+        
+        switch (gameResult) 
+        {
+            case 0:
+                document.querySelector('#result').textContent = 'You lost!';
+                grantPoint(gameResult);
+                break;
+            case 1:
+                document.querySelector('#result').textContent = 'You won!';
+                grantPoint(gameResult);
+                break;
+            case 2:
+                document.querySelector('#result').textContent = 'You tied!';
+                grantPoint(gameResult);
+                break;
+        }
+    });
+}
+
+
+
+
+
+//DEBUGGER CODE
+const debug = document.querySelector('#title');
+    debug.addEventListener('click', () => {
+        console.log(picker.length)
+    });
+
